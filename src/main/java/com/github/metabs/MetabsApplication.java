3cd82fc58ad776397ab94592c16026ef9a193356
Farina, Antonio <antonio.farina@ibm.com>
2022-02-22 16:36:57 +0100

    Initial commit

---
+++ src/main/java/com/github/metabs/MetabsApplication.java
@@ -0,0 +1,13 @@
+package com.github.metabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/metabs/MetabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.metabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
