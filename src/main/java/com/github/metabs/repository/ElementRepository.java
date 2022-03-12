a8e747f04a5ec494aeec68e9d8d1de015eef91fd
Damiano Petrungaro <damiano@pento.io>
2022-03-12 19:22:56 +0100

    ci: refactor github actions

--- src/main/java/com/github/metabs/repository/ElementRepository.java
+++ src/main/java/com/github/metabs/repository/ElementRepository.java
@@ -11,16 +11,12 @@ import org.springframework.stereotype.Repository;
 public interface ElementRepository extends Neo4jRepository<Element, UUID> {
 
   @Query(
-      "MATCH p=(child:Element{id:$id, trashed: null})"
-          + "-[:HAS_PARENT*0..1]->(parent:Element{trashed: null}) "
+      "MATCH p=(child:Element{id:$id})-[:HAS_PARENT*0..1]->(parent:Element) "
           + "RETURN child, collect(relationships(p)), collect(nodes(p))"
   )
   Optional<Element> findById(UUID id);
 
-  @Query(
-      "MATCH (n:Element {id:$id})"
-          + "DELETE n"
-  )
+  @Query("MATCH (n:Element {id:$id}) DELETE n")
   void deleteById(UUID id);
 }
 
